e9d39c3eefa1908d7784e4d7873c71cb68378b55
TheoFj <theo.feijoo@gmail.com>
2024-05-30 13:29:57 +0200

    nouvelles images

--- Algo_etoiles/main.py
+++ Algo_etoiles/main.py
@@ -13,7 +13,7 @@ FOV = 90
 L = FOV*pi/180 #fov en radians 
 L2 = (L**2)/2 #rayon du disque des etoiles prises en compte dans "calcul_gnomic_dtf_tfl" (cf. 4.1 https://www.mdpi.com/1424-8220/20/11/3027)
 LAMBD = 50 #nombre maximum d'etoiles de "reference"
-IMAGE_PATH = "Algo_etoiles/images/UMa2.png"
+IMAGE_PATH = "Algo_etoiles/images/UMa4.png"
 DATA_BASE_PATH = "Algo_etoiles/database/UMa_vmagmax6.csv"
 TEMP_IMAGE_PATH = "temp.png"
 CENTROID_SAVE_PATH = "resultats/centroids.png"
@@ -28,7 +28,7 @@ IMPORTATION ET TRAITEMENT D'IMAGE
 DISPLAY_CIRCLE_RADIUS = 6 #rayon des cercles lors de l'affichage des etoiles trouvees
 FONT = ImageFont.truetype(font = "arial.ttf", size = 12)
 
-BLACK_WHITE_THRESHOLD = 230
+BLACK_WHITE_THRESHOLD = 190
 def pixel_to_NB(pixel): #permet de choisir le contraste de la conversion en noir et blanc
     return 1 if pixel > BLACK_WHITE_THRESHOLD else 0
 
